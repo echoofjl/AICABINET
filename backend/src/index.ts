a575bef8e3c09cb30df4fcba8f1532d17d4b4ceb
JL <108511164+jessicali1625@users.noreply.github.com>
2025-05-08 09:17:37 +0800

    jl #20250506 add error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import promptRoutes from './routes/promptRoutes.js';
 import textRefinerRoutes from './routes/textRefinerRoutes.js';
+import { errorHandler } from './utils/errorHandler.js';
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/prompt", promptRoutes);
 app.use("/api", textRefinerRoutes);
+app.use(errorHandler);
 
 
 const port = process.env.PORT || 3000;
